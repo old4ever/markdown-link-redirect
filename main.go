74e07cc2cd6118829b839b3b567c218d71db78ef
Dmytro Stanchiev <git@old4ever.simplelogin.com>
2024-06-23 23:39:59 -0400

    feat: http server to redirect to magnet links for markdown imbedding

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	// "errors"
+)
+
+func main() {
+	r := gin.Default()
+
+	// r.GET("/ping", func(ctx *gin.Context) {
+	// 	ctx.JSON(http.StatusOK, gin.H{
+	// 		"message": "pong",
+	// 	})
+	// })
+
+	// r.GET("/redirect", func(ctx *gin.Context) {
+	// 	ctx.Redirect(http.StatusFound, "/ping")
+	// })
+
+	// r.GET("/query", func(ctx *gin.Context) {
+	// 	hash := ctx.Query("hash")
+	// 	if hash == "" {
+	// 		ctx.String(http.StatusBadRequest, "hash query parameter is missing")
+	// 	} else {
+	// 		ctx.String(http.StatusOK, "Hash is: %s", hash)
+	// 	}
+	// })
+
+	r.GET("/magnet-redirect", func(ctx *gin.Context) {
+		hash := ctx.Query("hash")
+		magnet := fmt.Sprintf("magnet:?xt=urn:btih:%s", hash)
+		if hash == "" {
+			ctx.String(http.StatusBadRequest, "hash query parameter is missing")
+		} else {
+			ctx.Redirect(http.StatusFound, magnet)
+		}
+	})
+
+	r.Run(":80")
+}
